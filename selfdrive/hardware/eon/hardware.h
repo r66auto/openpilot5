3ca1c7975a16bfc966379bb744638239c42d1e13
openpilotusers <>
2021-07-23 01:32:32 +0900

    openpilot v0.8.7 release

--- selfdrive/hardware/eon/hardware.h
+++ selfdrive/hardware/eon/hardware.h
@@ -15,7 +15,7 @@ class HardwareEon : public HardwareNone {
 public:
   static constexpr float MAX_VOLUME = 1.0;
   static constexpr float MIN_VOLUME = 0.5;
-  static constexpr float VAR_VOLUME = std::stof(Params().get("OpkrUIVolumeBoost")) * 0.01;
+  float VAR_VOLUME = std::stof(Params().get("OpkrUIVolumeBoost")) * 0.01;
 
   static bool EON() { return true; }
   static std::string get_os_version() {
