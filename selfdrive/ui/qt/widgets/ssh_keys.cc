ab91ef52555b77ac9df87202b87fd3e1f268a1fd
sunnyhaibin <haibin.wen3@gmail.com>
2021-07-23 23:53:28 -0400

    Fix ssh_keys.cc: missing header

--- selfdrive/ui/qt/widgets/ssh_keys.cc
+++ selfdrive/ui/qt/widgets/ssh_keys.cc
@@ -11,6 +11,7 @@
 #include "selfdrive/common/params.h"
 #include "selfdrive/ui/qt/api.h"
 #include "selfdrive/ui/qt/widgets/input.h"
+#include "selfdrive/ui/ui.h"
 
 SshControl::SshControl() : AbstractControl("SSH Keys", "Warning: This grants SSH access to all public keys in your GitHub settings. Never enter a GitHub username other than your own. A comma employee will NEVER ask you to add their GitHub username.", "") {
 
