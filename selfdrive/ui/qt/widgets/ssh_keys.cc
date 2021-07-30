15b64d8dd4f1daf0199e7a138906b3419d41edf5
Vehicle Researcher <>
2021-07-30 15:09:14 +0900

    Revert "openpilot v0.8.7 release"
    This reverts commit 82be5ad23ba120836bdbea1da57fd1267cab5fd0.

--- selfdrive/ui/qt/widgets/ssh_keys.cc
+++ selfdrive/ui/qt/widgets/ssh_keys.cc
@@ -2237,11 +2237,8 @@ void SRBaseControl::refresh() {
   sr_base_value.setText(QString::fromStdString(valuefs.toStdString()));
 }
 void SRBaseControl::vedit(QString str) {
-  float valuef = str.toFloat() * 100;
-  int valuei = valuef.toInt();
-  QString valuefs = QString::number(valuei);
-  params.put("SteerRatioAdj", valuefs.toStdString());
-  sr_base_value.setText(QString::fromStdString(str.toStdString()));
+  params.put("SteerRatioAdj", str.toStdString());
+  sr_base_value.setText(QString::fromStdString(valuefs.toStdString()));
 }
 
 SRMaxControl::SRMaxControl() : AbstractControl("SteerRatioMax", "SteerRatio 최대값을 설정합니다.", "../assets/offroad/icon_shell.png") {
