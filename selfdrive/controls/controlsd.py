7348c0c806583a3f855def144313d56ca51e11ab
R6auto <87813807+R6auto@users.noreply.github.com>
2021-11-28 06:36:13 +0400

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -533,7 +533,7 @@ class Controls:
     else:
       self.saturated_count = 0
 
-     Send a "steering required alert" if saturation count has reached the limit
+     #Send a "steering required alert" if saturation count has reached the limit
     if (lac_log.saturated and not CS.steeringPressed) or \
        (self.saturated_count > STEER_ANGLE_SATURATION_TIMEOUT):
 
