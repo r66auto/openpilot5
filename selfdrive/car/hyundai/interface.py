4c89c86a9f486fed9cd6686263cd334cd8330cbf
r66auto <85342156+r66auto@users.noreply.github.com>
2022-11-29 05:57:42 +0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -34,7 +34,7 @@ class CarInterface(CarInterfaceBase):
 
     ret.steerActuatorDelay = 0.1  # Default delay
     ret.steerRateCost = 0.5
-    #ret.steerLimitTimer = 0.4
+    ret.steerLimitTimer = 6000
     tire_stiffness_factor = 1.
 
     ret.stoppingControl = True
