49caa730f68876fb1cd42eda5a64079f4cd091c2
r66auto <85342156+r66auto@users.noreply.github.com>
2024-01-23 14:49:48 +0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -246,7 +246,7 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 3.01
       ret.steerRatio = 30.069
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.0], [0.0]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.13], [0.2]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.11], [0.1]]
   
 
     # these cars require a special panda safety mode due to missing counters and checksums in the messages
